d6d0f03154e5c5fe38df5468fd4157b52d558516
Tolulope Amole <amoletolulope@gmail.com>
2022-06-25 12:04:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let myName = "AMOLE Tolulope";
+let height = "1.82m";
+let country = "Nigeria";
+
+alert("My name is " + myName + "\n" + 
+        "I'm " + height + " tall" + "\n" +
+        "I'm from " + country
+);
+const root = document.getElementById("root");
+root.onclick = function(){
+ alert("My name is " + myName + "\n" + 
+        "I'm " + height + " tall" + "\n" +
+        "I'm from " + country
+);
+   
+}
+
